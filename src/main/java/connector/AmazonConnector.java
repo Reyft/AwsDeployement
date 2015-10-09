c87e89087e2422f840744d9782cdd931ddbc1822
rémy dupanloup <dupanloup.remy@etu.unice.fr>
2015-10-09 16:52:48 +0200

    YEAH³ !

--- src/main/java/connector/AmazonConnector.java
+++ src/main/java/connector/AmazonConnector.java
@@ -7,13 +7,13 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
-import com.amazonaws.services.dynamodbv2.model.BatchWriteItemRequest;
 import com.amazonaws.services.dynamodbv2.util.Tables;
-import com.amazonaws.services.rds.model.SourceType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -21,6 +21,7 @@ import com.amazonaws.services.rds.model.SourceType;
  */
 public class AmazonConnector {
     public static String usernane = "Remy";
+    public static int compt = 3;
 
     public static void main(String [] args){
         ProfileCredentialsProvider provider = new ProfileCredentialsProvider(usernane);
@@ -35,17 +36,50 @@ public class AmazonConnector {
             Table ld = dynamoDB.getTable("libraryDesign");
             Item book = new Item();
             book.withPrimaryKey("productID", 1).with("title", "Harry Potter")
-                    .with("year", 11).with("nbPage", 400);
+                    .with("year", 11).with("nbPage", 854);
             ld.putItem(book);
 
             QuerySpec query1 = new QuerySpec().withKeyConditionExpression("productID = :Id")
                     .withValueMap(new ValueMap().withNumber(":Id", 1));
 
+            Map<String, String> param = new HashMap();
+            param.put("Hall", "Berry");
+            param.put("Johny", "Depp");
+            Item dvd = new Item();
+            dvd.withPrimaryKey("productID", 2).with("title", "Mission Impossible").with("time", 90).with("actors", param);
+            ld.putItem(dvd);
+
+            System.out.println("<===== Question 4 =====>");
+
             for(Item i : ld.query(query1)) {
                 System.out.println(i.toJSONPretty());
             }
 
-            System.out.println("Job's done !");
+            QuerySpec query2 = new QuerySpec().withKeyConditionExpression("productID = :Id")
+                    .withValueMap(new ValueMap().withNumber(":Id", 2));
+
+            for(Item i : ld.query(query2)) {
+                System.out.println(i.toJSONPretty());
+            }
+
+            System.out.println("<===== Question 5 =====>");
+
+            for (Item i : ld.scan()){
+                System.out.println(i.toJSONPretty());
+            }
+
+            System.out.println("<===== Question 6 =====>");
+
+            long time = System.currentTimeMillis();
+            for (;compt < 2000; compt++){
+                Item prout = new Item().withPrimaryKey("productID", compt).with("try", compt);
+                ld.putItem(prout);
+                System.out.println(compt+": "+((new Double((double)1/((System.currentTimeMillis()-time)/1000.0))))
+                        .toString()+"/s");
+                time=System.currentTimeMillis();
+            }
+
+            System.out.println("\nJob's done !");
         }
     }
 
