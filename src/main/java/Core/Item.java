c0ee7384974aa1f9883b3ead1dfbf6c73cccec94
rémy dupanloup <dupanloup.remy@etu.unice.fr>
2015-10-09 15:40:48 +0200

    yeah !

--- src/main/java/Core/Item.java
+++
@@ -1,7 +0,0 @@
-package Core;
-
-/**
- * Created by remy on 09/10/15.
- */
-public class Item {
-}
--- src/main/java/connector/AmazonConnector.java
+++ src/main/java/connector/AmazonConnector.java
@@ -6,7 +6,14 @@ import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.BatchWriteItemRequest;
 import com.amazonaws.services.dynamodbv2.util.Tables;
+import com.amazonaws.services.rds.model.SourceType;
 
 
 /**
@@ -14,8 +21,10 @@ import com.amazonaws.services.dynamodbv2.util.Tables;
  */
 public class AmazonConnector {
     public static String usernane = "Remy";
+
     public static void main(String [] args){
         ProfileCredentialsProvider provider = new ProfileCredentialsProvider(usernane);
+
         AWSCredentials credentials = provider.getCredentials();
         AmazonDynamoDBClient client = new AmazonDynamoDBClient(credentials);
         client.setRegion(Region.getRegion(Regions.EU_WEST_1));
@@ -23,7 +32,20 @@ public class AmazonConnector {
         DynamoDB dynamoDB = new DynamoDB(client);
 
         if (Tables.doesTableExist(client, "libraryDesign")){
+            Table ld = dynamoDB.getTable("libraryDesign");
+            Item book = new Item();
+            book.withPrimaryKey("productID", 1).with("title", "Harry Potter")
+                    .with("year", 11).with("nbPage", 400);
+            ld.putItem(book);
 
+            QuerySpec query1 = new QuerySpec().withKeyConditionExpression("productID = :Id")
+                    .withValueMap(new ValueMap().withNumber(":Id", 1));
+
+            for(Item i : ld.query(query1)) {
+                System.out.println(i.toJSONPretty());
+            }
+
+            System.out.println("Job's done !");
         }
     }
 
